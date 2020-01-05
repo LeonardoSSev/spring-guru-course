420c4dfd7c54cd38933e30422e69ac18a7d50083
Leonardo Santos Severino <leonardossev@gmail.com>
2020-01-05 20:14:36 -0300

    Adds the CrudRepository related to the Publisher entity

--- src/main/java/com/example/course/guru/repository/PublisherRepository.java
+++ src/main/java/com/example/course/guru/repository/PublisherRepository.java
@@ -1,4 +1,7 @@
 package com.example.course.guru.repository;
 
-public interface PublisherRepository {
+import com.example.course.guru.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
 }
