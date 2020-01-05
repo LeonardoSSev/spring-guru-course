549412b02b1fc8cb226d84a8ac30e66ceda853d9
Leonardo Santos Severino <leonardossev@gmail.com>
2020-01-05 20:40:34 -0300

    Implements the Author Controller with an endpoint that returns the authors template

--- src/main/java/com/example/course/guru/controller/AuthorController.java
+++ src/main/java/com/example/course/guru/controller/AuthorController.java
@@ -1,4 +1,23 @@
 package com.example.course.guru.controller;
 
+import com.example.course.guru.repository.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", this.authorRepository.findAll());
+
+        return "authors";
+    }
 }
